9c67e22cdc2912b46830fc5987d93ab097881587
BedrockDev <dev.bedrock@gmail.com>
2017-03-08 18:00:30 +0900

    first class for game programming

---
+++ Game Programming/Codes/ConsoleApplication1/ConsoleApplication1/Pointer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+	int a = 100;
+
+	std::cout << "Value stored in a : " << a << std::endl;
+	std::cout << "Pointer value in a : " << &a << std::endl;
+
+	return 0;
+}
