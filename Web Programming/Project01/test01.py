4b7bde8f8ce1da2107e395d176d8d707a798eaff
Nathan Cho <dev.bedrock@gmail.com>
2017-03-13 12:35:07 +0900

    chapter 3

--- Web Programming/Project01/test01.py
+++ Web Programming/Project01/test01.py
@@ -1,9 +1,10 @@
-print "verse 15, Exercise 2.2"
+# Chapter 1, 2
+print "Exercise 2.2"
 name = raw_input("Enter your name : ")
 
 print "Hello " + name
 
-print "verse 15, Exercise 2.3"
+print "Exercise 2.3"
 workHours = int(raw_input("Enter Hours : "))
 workRate = float(raw_input("Enter Rate : "))
 workPay = float(workHours * workRate)
@@ -11,7 +12,7 @@ workPay = float(workHours * workRate)
 print "Your pay is : " + str(workPay)
 # could use round() function to round up to second place
 
-print "verse 15, Exercise 2.4"
+print "Exercise 2.4"
 width = 17
 height = 12.0
 
@@ -25,7 +26,7 @@ print check2  # float (8.5)
 print check3  # float (4.0) -> at least one calculating / calculated value is value
 print check4  # integer (11)
 
-print "verse 15, Exercise 2.5"
+print "Exercise 2.5"
 celsius = float(raw_input("Enter celsius : "))
 print celsius
 fahrenheit = float(celsius * 9 / 5) + 32
