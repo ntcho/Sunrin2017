34c549bac536632ab728a8238766626638ffd610
Nathan Cho <dev.bedrock@gmail.com>
2017-03-13 10:31:39 +0900

    java idk program

--- Programming/src/com/company/Main.java
+++ Programming/src/com/company/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         int a = 10, b = 15;
-        System.out.print(a * b);
+        // prints with auto line break
+        System.out.println(a * b);
+        // prints with no line break
+        System.out.print(a - b);
     }
 }
