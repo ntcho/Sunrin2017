dba7efcbc8c16f96596583afc55b492ce0492dd8
Nathan Cho <dev.bedrock@gmail.com>
2017-03-13 09:35:40 +0900

    test java program

---
+++ Programming/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int a = 10, b = 15;
+        System.out.print(a * b);
+    }
+}
