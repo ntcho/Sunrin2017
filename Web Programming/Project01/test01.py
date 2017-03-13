a3a77de1237a85d7cbffbe3dcd66cb0f07116803
Nathan Cho <dev.bedrock@gmail.com>
2017-03-13 11:54:51 +0900

    problem edited

--- Web Programming/Project01/test01.py
+++ Web Programming/Project01/test01.py
@@ -12,18 +12,18 @@ print "Your pay is : " + str(workPay)
 # could use round() function to round up to second place
 
 print "verse 15, Exercise 2.4"
-width = float(raw_input("Width : "))
-height = float(raw_input("Height : "))
+width = 17
+height = 12.0
 
 # data types for following values
 check1 = width / 2
 check2 = width / 2.0
 check3 = height / 3
 check4 = 1 + 2 * 5
-print check1  # integer
-print check2  # float
-print check3  # integer
-print check4  # integer
+print check1  # integer (8) -> all integer
+print check2  # float (8.5)
+print check3  # float (4.0) -> at least one calculating / calculated value is value
+print check4  # integer (11)
 
 print "verse 15, Exercise 2.5"
 celsius = float(raw_input("Enter celsius : "))
