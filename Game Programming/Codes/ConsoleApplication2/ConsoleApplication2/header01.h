f3999f9bda7115a02198dab8b439d088ac095b1e
BedrockDev <dev.bedrock@gmail.com>
2017-03-14 11:58:34 +0900

    splitting functions to different files and header files

---
+++ Game Programming/Codes/ConsoleApplication2/ConsoleApplication2/header01.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace SunrinHigh {
+	void StudentInfo(void);
+}
+
+namespace SunrinMiddle {
+	void StudentInfo(void);
+}
---
+++ Game Programming/Codes/ConsoleApplication2/ConsoleApplication2/test02-2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "header01.h"
+
+void SunrinHigh::StudentInfo(void) {
+	std::cout << "Function contains SunrinHigh" << std::endl;
+}
+
+void SunrinMiddle::StudentInfo(void) {
+	std::cout << "Function contains SunrinMiddle" << std::endl;
+}
