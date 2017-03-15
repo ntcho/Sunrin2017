e8c5eb14af2717fc37538ea114503de5e4676fba
BedrockDev <dev.bedrock@gmail.com>
2017-03-15 11:16:10 +0900

    recomposited file structure and 2nd class

---
+++ Programming/Java02/src/com/bedrock/Main.java
@@ -0,0 +1,10 @@
+package com.bedrock;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (String word : args) {
+            System.out.print(word);
+        }
+    }
+}
