58f461299d64af01f38c929d34e343b25b0aeedd
BedrockDev <dev.bedrock@gmail.com>
2017-03-16 13:59:36 +0900

    String data type / cin input functions

---
+++ Game Programming/Codes/consoleapplication4/ConsoleApplication4/DataTypeString.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	string s1 = "Good";
+	string s2 = "Morning";
+	string s3 = s1 + " " + s2 + "!\n";
+	cout << s3;
+
+	return 0;
+}
---
+++ Game Programming/Codes/consoleapplication4/ConsoleApplication4/InputCinGet.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	char ch1, ch2;
+
+	ch1 = cin.get();
+	ch2 = cin.get();
+	// inputs 1 character
+
+	cout << ch1 << " " << ch2 << endl;
+	return 0;
+}
