dfd54b9d771c1bd0fd539336863e24170a5e91c8
BedrockSunrin <dev.bedrock@gmail.com>
2017-03-20 10:41:22 +0900

    prompt window io

--- Programming/Java04/src/Exam.java
+++ Programming/Java04/src/Exam.java
@@ -5,7 +5,7 @@ public class Exam {
 		System.out.println("3 + 4"); // the entire string "3 + 4"
 		System.out.println(3 + 4); // calculated value of 3 + 4 = 7
 		System.out.println(3 + 4 + "4"); // calculated value of 3 + 4 = 7 and string "4" merged
-		System.out.println("3" + 3 + 4); // string "3" and the two following operands converted to seperated string "3" and "4" merged
+		System.out.println("3" + 3 + 4); // string "3" and the two following operands converted to separated string "3" and "4" merged
 	}
 
 }
