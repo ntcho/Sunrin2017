0537c291db4633e10a5183fbce7a26dd1fa2827d
BedrockSunrin <dev.bedrock@gmail.com>
2017-03-20 11:27:34 +0900

    function not defined errors (exercise)

---
+++ Web Programming/Project02/exercise_problems.py
@@ -0,0 +1,14 @@
+def print_lyrics():
+    print "I'm a lumberjack, and I'm okay"
+    print "I sleep all night and I work all day"
+
+
+# repeat_lyrics()
+# return an error: "NameError: name 'repeat_lyrics' is not defined"
+
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+repeat_lyrics()
