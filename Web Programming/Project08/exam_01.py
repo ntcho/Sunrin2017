a97acc6dd69e7a1efe4bded9bd164c7004eed4a9
BedrockSunrin <dev.bedrock@gmail.com>
2017-05-22 11:40:59 +0900

    Added python files

---
+++ Web Programming/Project08/exam_01.py
@@ -0,0 +1,22 @@
+# page 113
+
+import string
+
+fname = raw_input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print 'File cannot be opened:', fname
+    exit()
+counts = dict()
+for line in fhand:
+    # this changes all character in the given string to the following argument
+    line = line.translate(None, string.punctuation)
+    line = line.lower()
+    words = line.split()
+    for word in words:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
+print counts
