3459914068fa350d4fcacc7d8f7606de06ab2dac
BedrockSunrin <dev.bedrock@gmail.com>
2017-05-25 14:08:43 +0900

    Finished RedDog game

--- Game Programming/Codes/ConsoleApplication15/ConsoleApplication15/RedDog.cpp
+++ Game Programming/Codes/ConsoleApplication15/ConsoleApplication15/RedDog.cpp
@@ -23,7 +23,7 @@ public:
 		if (isUsed == false) {
 			switch (shape) {
 			case 0:
-				cout << "♦";
+				cout << "◆";
 				break;
 			case 1:
 				cout << "♣";
@@ -57,9 +57,17 @@ public:
 			isUsed = true;
 		}
 	}
+
+	int getNumber() {
+		return number;
+	}
 };
 
+int money = 1000;
+int totalCount = 0;
+
 int main() {
+	// initialize the card
 	Card card[52];
 	for (int i = 0; i < 4; i++) {
 		for (int j = 0; j < 13; j++) {
@@ -78,5 +86,85 @@ int main() {
 		tmp = card[cng];
 		card[cng] = card[src];
 		card[src] = tmp;
+	}
+
+	int count = 0;
+	int bet;
+	while (true) {
+		if (count >= 17) {
+			cout << "No cards left, money left $" << money << endl;
+			while (true) {
+				cout << "Do you want to try again? (Y/N) : ";
+				char selection;
+				cin >> selection;
+				if (selection == 'Y' || selection == 'y') {
+					cout << "Suffling the cards again..." << endl;
+					main();
+				}
+				else if (selection == 'N' || selection == 'n') {
+					break;
+				}
+			}
+			break;
+		}
+		else if (money <= 0) {
+			cout << "No money left, you lose." << endl;
+			break;
+		}
+		cout << "----- Turn " << totalCount + 1 << " -----" << endl;
+		while (true) {
+			cout << "Current money : $" << money << "\t Money to bet : $";
+			cin >> bet;
+			if (bet <= 0) {
+				cout << "You need to bet at least $1" << endl;
+			}
+			else if (bet > money) {
+				cout << "You don't enough money to bet" << endl;
+			}
+			else if (bet == money) {
+				cout << "Are you sure you are all in? (Y/N) : ";
+				char selection;
+				cin >> selection;
+				if (selection == 'Y' || selection == 'y') {
+					cout << "All in." << endl;
+					break;
+				}
+			}
+			else {
+				break;
+			}
+		}
+		money -= bet;
+		// money bet, show cards
+		Card card1 = card[count * 3];
+		Card card2 = card[(count * 3) + 1];
+		Card card3 = card[(count * 3) + 2];
+
+		card1.printCard();
+		card2.printCard();
+		card3.printCard();
+		//cout << "\t";
+
+		if (card1.getNumber() > card3.getNumber() &&
+			card2.getNumber() < card3.getNumber()) {
+			// win
+			cout << " Won  : recieved $" << bet * ((count + 1) * 2) << endl;
+			money += bet * ((count + 1) * 2);
+		}
+		else if (
+			(card1.getNumber() == card3.getNumber() &&
+			card2.getNumber() < card3.getNumber()) || 
+			(card1.getNumber() > card3.getNumber() &&
+			card2.getNumber() == card3.getNumber())) {
+			// duel
+			cout << " Duel : recieved $" << bet << endl;
+			money += bet;
+		}
+		else {
+			// lose
+			cout << " Lose : lost $" << bet << endl;
+		}
+		count++;
+		totalCount++;
 	}
 }
