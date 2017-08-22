7857c83d8be41ca5dc5738bc5d3e99553ebe8b81
BedrockSunrin <dev.bedrock@gmail.com>
2017-08-22 12:13:15 +0900

    Added ball roll unity project

---
+++ Game Programming/Projects/Example_Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    // this variable could be changed in unity inspector window
+    public float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+	}
+}
