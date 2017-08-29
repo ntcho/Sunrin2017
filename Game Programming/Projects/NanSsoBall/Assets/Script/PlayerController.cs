8075d3708f3944aeda5a1e1c07693d0f26194b93
BedrockSunrin <dev.bedrock@gmail.com>
2017-08-29 11:35:06 +0900

    Added Unity ball project

---
+++ Game Programming/Projects/NanSsoBall/Assets/Script/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Text countText;
+    public Text winText;
+
+    private Rigidbody rb;
+    public float speed;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 3)
+            winText.text = "You won!";
+    }
+}
