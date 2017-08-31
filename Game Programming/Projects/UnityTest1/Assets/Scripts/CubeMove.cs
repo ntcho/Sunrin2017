7f4f1463969be53938aae69f6520acd664ed68e6
BedrockSunrin <dev.bedrock@gmail.com>
2017-08-31 14:13:41 +0900

    Added Unity script basics

---
+++ Game Programming/Projects/UnityTest1/Assets/Scripts/CubeMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    float Dir = 0.2f;
+	
+	// Update is called once per frame
+	void Update () {
+        // this prints the position of the cube object
+        //print(GetComponent<Transform>().position);
+        transform.position = transform.position + new Vector3(Dir, 0, 0);
+
+        // calculate the time between frame and frame
+
+        if (transform.position.x > 5)
+        {
+            Dir = -0.2f;
+        }
+        if (transform.position.x < -5)
+        {
+            Dir = 0.2f;
+        }
+	}
+}
---
+++ Game Programming/Projects/UnityTest1/Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Time.deltaTime is the frame interval time
+        transform.Rotate(0, 90 * Time.deltaTime, 0);
+	}
+}
