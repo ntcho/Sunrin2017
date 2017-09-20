b9aa838024ecf71fc7a7a730ec194e2e6a116011
BedrockSunrin <dev.bedrock@gmail.com>
2017-09-20 11:57:11 +0900

    Modified chatroom ArrayList

--- Programming/Java31/src/Chatroom.java
+++ Programming/Java31/src/Chatroom.java
@@ -1,21 +1,25 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+//import java.util.Arrays;
 
 public class Chatroom {
-	
-	private Enterable persons[];
+
+	private ArrayList<Enterable> persons;
+	// private Enterable persons[];
 	
 	private int index;
 	
 	public Chatroom(int size) {
 		// student-only chatroom
-		persons = new Student[size];
+		persons = new ArrayList<>();
+		//persons = new Student[size];
 		index = 0;
 		print("Made chatroom of " + size + " people(s)");
 	}
 	
 	public void enter(Object person) {
 		if (person instanceof Enterable) {
-			persons[index++] = (Enterable) person;
+			persons.add((Enterable) person);
+			//persons[index++] = (Enterable) person;
 			print(person + " \t entered");
 		} else {
 			print(person + " \t denied");
@@ -24,7 +28,8 @@ public class Chatroom {
 	
 	@Override
 	public String toString() {
-		return "Chatroom [" + persons.length + "], " + Arrays.toString(persons); 
+		//return "Chatroom [" + persons.length + "], " + Arrays.toString(persons); 
+		return "Chatroom [" + persons.size() + "], " + persons;
 	}
 	
 	public static void print(Object obj) {
